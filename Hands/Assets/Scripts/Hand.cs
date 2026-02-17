ea62cef83dbfc63e4e916a017fc1aaa90cc060c2
saaralaasone <71126251+saaralaasone@users.noreply.github.com>
2026-02-17 14:02:53 +0200

    Update
    Fixed the animation (now works), added new material for cube to indicate that in can be picked up (can be removed later)

--- Hands/Assets/Scripts/Hand.cs
+++ Hands/Assets/Scripts/Hand.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
 {
-    public float speed;
-    Animator animator;
+    //animation
+    [SerializeField] private float speed;
+    private Animator animator;
     private float gripTarget;
     private float triggerTarget;
     private float gripCurrent;
@@ -15,15 +16,56 @@ public class Hand : MonoBehaviour
     private string animatorGripParam = "Grip";
     private string animatorTriggerParam = "Trigger";
 
+    //physics movement
+    [SerializeField] private GameObject followObject;
+    [SerializeField] private float followSpeed = 30f;
+    [SerializeField] private float rotateSpeed = 100f;
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private Vector3 rotationOffset;
+    private Transform followTarget;
+    private Rigidbody body;
+
     void Start()
     {
+        //animation
         animator = GetComponent<Animator>();
+
+        //physics movement
+        followTarget = followObject.transform;
+        body = GetComponent<Rigidbody>();
+        body.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        body.interpolation = RigidbodyInterpolation.Interpolate;
+        body.mass = 20f;
+
+        //teleport hands
+        body.position = followTarget.position;
+        body.rotation = followTarget.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
         AnimateHand();
+        PhysicsMove();
+    }
+
+    private void PhysicsMove()
+    {
+        //position
+        var positionWithOffset = followTarget.TransformPoint(positionOffset); //followTarget.position + positionOffset;
+        var distance = Vector3.Distance(positionWithOffset, transform.position);
+        body.velocity = (positionWithOffset - transform.position).normalized * (followSpeed * distance);
+
+        //rotation
+        var rotationWithOffset = followTarget.rotation * Quaternion.Euler(rotationOffset);
+        var q = rotationWithOffset * Quaternion.Inverse(body.rotation);
+        q.ToAngleAxis(out float angle, out Vector3 axis);
+        if (Mathf.Abs(axis.magnitude)!=Mathf.Infinity)
+        {
+            if (angle>180.0f) { angle -= 360.0f; }
+            body.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
+        }
+
     }
 
     internal void SetGrip(float v)
